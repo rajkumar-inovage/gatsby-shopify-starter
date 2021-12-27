dad9bf6511211a605ac71ef35b9bad738d9d7f91
rajkumar-inovage <rajkumar@innovagesoftwares.com>
2021-12-27 22:13:01 +0530

    Signed-off-by: rajkumar-inovage <rajkumar@innovagesoftwares.com>

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,34 +1,44 @@
+require('dotenv').config({
+  path: `.env.${process.env.NODE_ENV}`,
+});
+const path = require('path');
 module.exports = {
   siteMetadata: {
-    siteUrl: "https://www.yourdomain.tld",
-    title: "gatsby-shopify-starter",
+    title: `Gatsby Shopify Starter`,
+    description: `🛒 Theme to build a blazing simple and fast store with Gatsby and Shopify.`,
+    author: `raj-inovexia`,
+    siteUrl: process.env.SITE_URL,
   },
   plugins: [
+    'gatsby-plugin-image',
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-sitemap',
+    'gatsby-plugin-sharp',
+    'gatsby-transformer-sharp',
     {
-      resolve: "gatsby-source-shopify",
+      resolve: 'gatsby-source-graphql',
       options: {
-        shopName: "inovexia-store.myshopify.com",
-        accessToken: "",
+        // This type will contain remote schema Query type
+        typeName: 'ShopifyStore',
+        // This is field under which it's accessible
+        fieldName: 'store',
+        // Url to query from
+        url: `https://${process.env.SHOP_NAME}.myshopify.com/api/2022-01/graphql.json`,
+        headers: {
+          // Learn about environment variables: https://gatsby.dev/env-vars
+          'X-Shopify-Storefront-Access-Token': `${process.env.SHOPIFY_ACCESS_TOKEN}`,
+          'Content-Type': 'application/graphql',
+        },
+        // refetchInterval: 60,
       },
     },
     {
-      resolve: "gatsby-plugin-google-analytics",
+      resolve: 'gatsby-source-filesystem',
       options: {
-        trackingId: "",
+        name: 'images',
+        path: './src/images/',
       },
-    },
-    "gatsby-plugin-image",
-    "gatsby-plugin-react-helmet",
-    "gatsby-plugin-sitemap",
-    "gatsby-plugin-sharp",
-    "gatsby-transformer-sharp",
-    {
-      resolve: "gatsby-source-filesystem",
-      options: {
-        name: "images",
-        path: "./src/images/",
-      },
-      __key: "images",
+      __key: 'images',
     },
   ],
 };
